e64c61596206d57c8ca23d01ad80f3d4b61381aa
jorgeviz <javg44@hotmail.com>
2018-04-06 19:04:04 -0500

    Web service structure

---
+++ app/tests/tests_service.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+class CatalogueServiceTestCase(unittest.TestCase):
+    """ Test Case for Catalogue Service
+    """
+
+    def test_catalogue_connection(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
