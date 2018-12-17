6789b4677bff2bb48e5236080052e3875e392ebd
Krakkev <kevangy@hotmail.com>
2018-12-17 12:02:37 -0600

    fixing bugs

--- scripts/clean_category.py
+++ scripts/clean_category.py
@@ -637,16 +637,20 @@ def get_categories_related(categories_raw, min_score=90, min_bad_score=80, is_na
 
 def create_categories_in_db():
     print("Creating categories in db")
+    db_name = os.getenv("SQL_DB")
+    if os.getenv('ENV', '').lower() == 'dev' and 'dev' not in os.getenv("SQL_DB", ''):
+        db_name += '_dev'
+
     db = Pygres(
         {
             "SQL_HOST": os.getenv("SQL_HOST"),
             "SQL_PORT": os.getenv("SQL_PORT"),
-            "SQL_DB": os.getenv("SQL_DB") + '_dev' if (os.getenv('ENV', '').lower() == 'dev' and 'dev' not in os.getenv("SQL_DB")) else os.getenv("SQL_DB"),
+            "SQL_DB": db_name,
             "SQL_USER": os.getenv("SQL_USER"),
             "SQL_PASSWORD": os.getenv("SQL_PASSWORD")
         }
     )
-    if
+
     bp_farma = pd.read_sql("select * from category where source='byprice_farma'", db.conn)
     bp_all = pd.read_sql("select id_category from category where source='byprice'", db.conn)
     bp_source = pd.read_sql("select * from source where key='byprice_farma'", db.conn)
