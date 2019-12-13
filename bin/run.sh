734f96f4695dbba5a7cd1be9077e4d97b2232355
yogesh <yogesh@byprice.com>
2019-12-13 13:38:50 -0600

    sh log

--- bin/run.sh
+++ bin/run.sh
@@ -13,7 +13,7 @@ pipenv run flask initdb
 is_running_gunicorn=$(ps aux | grep 'gunicorn' | wc -l)
 if [ $is_running_gunicorn -gt 1 ]
     then
-            echo "[$(date)][CATALOGUE]: Already running with GUNICORN. Shutting down"
+            echo "[$(date)][$APP_NAME]: Already running with GUNICORN. Shutting down"
             ps aux | grep 'gunicorn'
             kill $(ps aux | grep 'gunicorn' | awk '{print $2}')
 fi
@@ -21,10 +21,11 @@ fi
 is_running_flask_run=$(ps aux | grep 'flask consumer' | wc -l)
 if [ $is_running_flask_run -gt 1 ]
     then
-            echo "[$(date)][CATALOGUE]: Already running with FLASK CONSUMER. Shutting down"
+            echo "[$(date)][$APP_NAME]: Already running with FLASK CONSUMER. Shutting down"
             ps aux | grep 'flask consumer'
             kill $(ps aux | grep 'flask consumer' | awk '{print $2}')
 fi
 
+echo "[$(date)][$APP_NAME]: Starting $APP_NAME with $MODE mode."
 pipenv run flask consumer
 
